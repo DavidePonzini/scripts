d527a09f342bfe5bf4fcf45b0d2b5ec50e686fb3
Davide Ponzini <davide.ponzini95@gmail.com>
2023-12-13 12:25:16 +0100

    install packages

--- py-install-packages.py
+++ py-install-packages.py
@@ -2,6 +2,9 @@
 
 from dav_tools import messages, commands
 
+commands.execute('python3 -m pip install --upgrade pip')
+messages.success('Updated pip to latest version')
+
 commands.execute('pipreqs --mode no-pin --force')
 messages.info('Generated requirements file')
 
