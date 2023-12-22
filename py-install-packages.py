fead283027810997178ed121a85d5a4f071e5c75
Davide Ponzini <davide.ponzini95@gmail.com>
2023-12-22 16:12:34 +0100

    update pip

--- py-install-packages.py
+++ py-install-packages.py
@@ -2,9 +2,6 @@
 
 from dav_tools import messages, commands
 
-commands.execute('python3 -m pip install --upgrade pip')
-messages.success('Updated pip to latest version')
-
 commands.execute('pipreqs --mode no-pin --force')
 messages.info('Generated requirements file')
 
--- update.py
+++ update.py
@@ -4,61 +4,77 @@ import platform
 
 from dav_tools import commands, messages, requirements, argument_parser, ArgumentAction
 
+def run_update(func, *args, **kwargs) -> None:
+    '''
+    Helper function to run an update and handle exceptions
+    
+    :param func: Function to run.
 
-def update_linux():
+    :returns: None
+    '''
     try:
-        if argument_parser.args.update:
-            commands.execute('apt update')
-            messages.success('Downloaded package information')
-        else:
-            messages.info('Skipping update')
-
-        if argument_parser.args.download_only:
-            commands.execute('apt dist-upgrade -y --download-only')
-            messages.success('Downloaded package upgrades')
-        else:
-            commands.execute('apt dist-upgrade -y')
-            messages.success('Installed package upgrades')
-
-        if argument_parser.args.cleanup:
-            commands.execute('apt autoremove -y')
-            messages.success('Removed unused packages')
-
-            commands.execute('apt autoclean')
-            messages.success('Cleaned up')
-        else:
-            messages.info('Skipping cleanup')
+        func(*args, **kwargs)
     except commands.CalledProcessError as e:
         messages.error(e)
     except KeyboardInterrupt:
         messages.warning('Interrupted')
 
+
+def update_linux(check_for_updates=True, download_only=False, cleanup=True):
+    if check_for_updates:
+        commands.execute('apt update')
+        messages.success('Downloaded package information')
+    else:
+        messages.info('Skipping check for updates')
+
+    if download_only:
+        commands.execute('apt dist-upgrade -y --download-only')
+        messages.success('Downloaded package upgrades')
+    else:
+        commands.execute('apt dist-upgrade -y')
+        messages.success('Installed package upgrades')
+
+    if cleanup:
+        commands.execute('apt autoremove -y')
+        messages.success('Removed unused packages')
+
+        commands.execute('apt autoclean')
+        messages.success('Cleaned up')
+    else:
+        messages.info('Skipping cleanup')
+    
 def update_windows():
-    try:
-        messages.progress('Installing prerequisites...')
-        commands.execute('powershell -command "Install-Module PSWindowsUpdate"')
-        commands.execute('powershell -command "Import-Module PSWindowsUpdate"')
-        messages.info('Installed prerequisites')
-        
-        messages.progress('Checking for updates...')
-        commands.execute('powershell -command "Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -Verbose"')
-        messages.success('Installed updates')
-    except commands.CalledProcessError as e:
-        messages.error(e)
-    except KeyboardInterrupt:
-        messages.warning('Interrupted')
+    messages.progress('Installing prerequisites...')
+    commands.execute('powershell -command "Install-Module PSWindowsUpdate"')
+    commands.execute('powershell -command "Import-Module PSWindowsUpdate"')
+    messages.info('Installed prerequisites')
+    
+    messages.progress('Checking for updates...')
+    commands.execute('powershell -command "Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -Verbose"')
+    messages.success('Installed updates')
+
+def update_python():
+    commands.execute('python -m pip install --upgrade pip')
+    messages.success('Updated pip to latest version')
 
 
 if __name__ == '__main__':
     argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
     argument_parser.set_description('Performs a full pc update')
-    argument_parser.add_argument('--update', help='check for updates', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
+    argument_parser.add_argument('--check-for-updates', help='check for updates', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
     argument_parser.add_argument('--download-only', help='download updates but skip installation', action=ArgumentAction.STORE_TRUE)
     argument_parser.add_argument('--cleanup', help='removed unnecessary packages', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
+    argument_parser.add_argument('--python', help='update python package manager', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
+
+    if(argument_parser.args.python):
+        run_update(update_python)
 
     requirements.require(root=True, os=['Linux', 'Windows'])
 
     if platform.system() == 'Linux':
-        update_linux()
+        run_update(update_linux,
+                   check_for_updates=argument_parser.args.check_for_updates,
+                   download_only=argument_parser.args.download_only,
+                   cleanup=argument_parser.args.cleanup)
     elif platform.system() == 'Windows':
-        update_windows()
+        run_update(update_windows)
