91e818865a0e954bcec8b6d7bcab8a17962df133
Davide Ponzini <davide.ponzini95@gmail.com>
2024-03-07 10:23:02 +0100

    removed some flags

--- update.py
+++ update.py
@@ -18,42 +18,42 @@ def run_update(func, *args, **kwargs) -> None:
         messages.error(e)
     except KeyboardInterrupt:
         messages.warning('Interrupted')
+        exit(1)
 
 
-def update_linux(check_for_updates=True, download_only=False, cleanup=True):
-    if check_for_updates:
-        commands.execute('apt update')
-        messages.success('Downloaded package information')
-    else:
-        messages.info('Skipping check for updates')
+def update_linux():
+    # upgrade
+    messages.info('Downloading package information...')
+    commands.execute('apt update')
+    messages.success('Downloaded package information')
 
-    if download_only:
-        commands.execute('apt dist-upgrade -y --download-only')
-        messages.success('Downloaded package upgrades')
-    else:
-        commands.execute('apt dist-upgrade -y')
-        messages.success('Installed package upgrades')
+    # update
+    messages.info('Installing package upgrades...')
+    commands.execute('apt dist-upgrade -y')
+    messages.success('Installed package upgrades')
 
-    if cleanup:
-        commands.execute('apt autoremove -y')
-        messages.success('Removed unused packages')
+    # autoremove
+    messages.info('Removing unused packages...')
+    commands.execute('apt autoremove -y')
+    messages.success('Removed unused packages')
 
-        commands.execute('apt autoclean')
-        messages.success('Cleaned up')
-    else:
-        messages.info('Skipping cleanup')
+    # autoclean
+    messages.info('Cleaning up...')
+    commands.execute('apt autoclean')
+    messages.success('Cleaned up')
     
 def update_windows():
-    messages.progress('Installing prerequisites...')
+    messages.info('Installing prerequisites...')
     commands.execute('powershell -command "Install-Module PSWindowsUpdate"')
     commands.execute('powershell -command "Import-Module PSWindowsUpdate"')
     messages.info('Installed prerequisites')
     
-    messages.progress('Checking for updates...')
+    messages.info('Checking for updates...')
     commands.execute('powershell -command "Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -Verbose"')
     messages.success('Installed updates')
 
 def update_python():
+    messages.info('Updating pip...')
     commands.execute('python -m pip install --upgrade pip')
     messages.success('Updated pip to latest version')
 
@@ -61,9 +61,6 @@ def update_python():
 if __name__ == '__main__':
     argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
     argument_parser.set_description('Performs a full pc update')
-    argument_parser.add_argument('--check-for-updates', help='check for updates', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
-    argument_parser.add_argument('--download-only', help='download updates but skip installation', action=ArgumentAction.STORE_TRUE)
-    argument_parser.add_argument('--cleanup', help='removed unnecessary packages', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
     argument_parser.add_argument('--python', help='update python package manager', action=ArgumentAction.BOOLEAN_OPTIONAL, default=True)
 
     requirements.require(root=True, os=['Linux', 'Windows'])
@@ -72,9 +69,7 @@ if __name__ == '__main__':
         run_update(update_python)
 
     if platform.system() == 'Linux':
-        run_update(update_linux,
-                   check_for_updates=argument_parser.args.check_for_updates,
-                   download_only=argument_parser.args.download_only,
-                   cleanup=argument_parser.args.cleanup)
+        run_update(update_linux)
     elif platform.system() == 'Windows':
         run_update(update_windows)
+    
