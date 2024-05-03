177e8541829c78e3ac293ca2fd4228c86d22e3f8
Davide Ponzini <davide.ponzini95@gmail.com>
2024-05-03 12:40:32 +0200

    removed api field no longer available

--- network/ip_info.py
+++ network/ip_info.py
@@ -10,7 +10,7 @@ def get_local_ip_address():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(('8.8.8.8', 80))
         return s.getsockname()[0]
-    
+
 def get_external_ip_info(ip=None):
     if ip is None:
         ip = requests.get('http://ipinfo.io/ip').content.decode()
@@ -20,7 +20,6 @@ def get_external_ip_info(ip=None):
 
     return result
 
-    
 
 if __name__ == '__main__':
     argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
@@ -39,10 +38,8 @@ if __name__ == '__main__':
     messages.info('Location:', f'{external_ip_info["city"]}, {external_ip_info["region"]} ({external_ip_info["postal"]}) - {external_ip_info["country"]}',
                   text_options=[[messages.TextFormat.Style.BOLD], []])
     messages.info('Coordinates:', external_ip_info['loc'], f'(https://www.google.com/maps/@{external_ip_info["loc"]})',
-                  text_options=[[messages.TextFormat.Style.BOLD], [], [messages.TextFormat.Style.ITALIC, messages.TextFormat.Color.DARKGRAY]])
+                  text_options=[[messages.TextFormat.Style.BOLD], [], [messages.TextFormat.Style.ITALIC, messages.TextFormat.Style.DIM]])
     messages.info('Timezone:', external_ip_info['timezone'],
                   text_options=[[messages.TextFormat.Style.BOLD], []])
     messages.info('Provider:', external_ip_info['org'],
-                  text_options=[[messages.TextFormat.Style.BOLD], []])
-    messages.info('Hostname:', external_ip_info['hostname'],
                   text_options=[[messages.TextFormat.Style.BOLD], []])
