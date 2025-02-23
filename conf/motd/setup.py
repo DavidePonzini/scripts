2151e1323d14b323b7eda7401493c60ba574477b
Davide Ponzini <davide.ponzini95@gmail.com>
2025-02-23 17:34:45 +0100

    Quick commit (2025-02-23)

--- conf/motd/setup.py
+++ conf/motd/setup.py
@@ -4,22 +4,27 @@ from dav_tools import requirements, commands, files, messages
 import sys
 
 
-requirements.require(
-   root=True,
-   os=[requirements.OS.LINUX],
-)
+def install_fail2ban():
+      files.copy_file(f'{sys.path[0]}/.files/88-fail2ban', '/etc/update-motd.d/88-fail2ban')
+      messages.info('Enabled fail2ban')
 
-# system info
-commands.execute('apt install -y landscape-common')
-messages.info('Enabled system info')
+def skip_fail2ban():
+      messages.info('fail2ban is not installed, skipping')
 
-# motd news
-files.copy_file(f'{sys.path[0]}/.files/motd-news', '/etc/default/motd-news')
-messages.info('Disabled news')
 
-# fail2ban
-if commands.get_output('which fail2ban-client', on_error=lambda: None) != '':
-   files.copy_file(f'{sys.path[0]}/.files/88-fail2ban', '/etc/update-motd.d/88-fail2ban')
-   messages.info('Enabled fail2ban')
-else:
-   messages.info('fail2ban is not installed, skipping')
+if __name__ == '__main__':
+   requirements.require(
+      root=True,
+      os=[requirements.OS.LINUX],
+   )
+
+   # system info
+   commands.execute('apt install -y landscape-common')
+   messages.info('Enabled system info')
+
+   # motd news
+   files.copy_file(f'{sys.path[0]}/.files/motd-news', '/etc/default/motd-news')
+   messages.info('Disabled news')
+
+   # fail2ban
+   commands.get_output('which fail2ban-client', on_success=install_fail2ban, on_error=skip_fail2ban)
