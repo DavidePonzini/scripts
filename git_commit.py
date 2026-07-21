3008743c5f3c93cedc9b772294092c7c3fde50d5
Davide Ponzini <davide.ponzini95@gmail.com>
2026-07-21 16:44:14 +0200

    amend option; tidied up

--- git_commit.py
+++ git_commit.py
@@ -5,11 +5,38 @@ import sys
 
 from dav_tools import argument_parser, commands, messages
 
+
+def git_add(*paths: str):
+    commands.execute(f'git add {" ".join(paths)}')
+    messages.success('Staged changes')
+
+def git_commit(amend: bool = False, message: str | None = None):
+    command = 'git commit --quiet'
+    if amend:
+        command += ' --amend'
+    if message:
+        command += f' -m "{message}"'
+    
+    try:
+        commands.execute(command)
+    except (KeyboardInterrupt, EOFError):
+        messages.warning('Aborted')
+        sys.exit(1)
+
+    messages.success('Committed changes')
+
+def git_push():
+    messages.progress('Uploading commit...')
+    commands.execute(f'git push --quiet')
+    messages.success('Uploaded commit')
+
+
 if __name__ == '__main__':
     argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
     argument_parser.set_description('Quickly commits and uploads all changes performed on the current directory')
     argument_parser.add_argument('path', help='files (or folders) to commit', nargs='*', default=[])
     argument_parser.add_argument('-m', '--message', help='commit message', default=None)
+    argument_parser.add_argument('-a', '--amend', help='amend', action='store_true')
     argument_parser.add_argument('-p', '--push', help='push changes to remote', action='store_true')
     argument_parser.add_argument('-v', '--verbose', help='show diff', action='store_true')
     argument_parser.parse_args()
@@ -19,8 +46,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     if len(argument_parser.args.path) > 0:
-        commands.execute(f'git add {" ".join(argument_parser.args.path)}')
-        messages.success('Staged changes')
+        git_add(*argument_parser.args.path)
 
     # display changes for the user
     if argument_parser.args.verbose:
@@ -30,21 +56,13 @@ if __name__ == '__main__':
 
     if commands.get_output('git diff --cached ') == b'':
         messages.warning('No changes staged for commit')
+
+        if argument_parser.args.push:
+            git_push()
+
         sys.exit(0)
 
-    if argument_parser.args.message is not None:
-        commit_message = argument_parser.args.message
-        commands.execute(f'git commit --quiet -m "{commit_message}"')
-    else:
-        try:
-            commands.execute('git commit')
-        except (KeyboardInterrupt, EOFError):
-            messages.warning('Aborted')
-            sys.exit(1)
-
-    messages.success('Committed changes')
+    git_commit(amend=argument_parser.args.amend, message=argument_parser.args.message)
 
     if argument_parser.args.push:
-        messages.progress('Uploading commit...')
-        commands.execute(f'git push --quiet')
-        messages.success('Uploaded commit')
+        git_push()
